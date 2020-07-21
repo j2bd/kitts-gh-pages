58d9d8cc19bfb6e621b19bcd1d2a911ef3481f26
JB Dubois <jeanbaptiste@re-ve.fr>
2020-07-21 11:03:48 +0200

    save

--- src/assets/js/cloudinaryjb.js
+++ src/assets/js/cloudinaryjb.js
@@ -7,16 +7,15 @@
   
 
 // insertion de mes images au fur et à mesure
-  var audi = cl.imageTag('logos/audi', {effect: "colorize", color: "#000000", height: 12, quality: 100, crop: "scale", format: "png"}).toHtml();
-  var getaround = cl.imageTag('logos/getaround', {effect: "colorize", color: "#000000", height: 30, quality: 100, crop: "scale", format: "png"}).toHtml();
-  var ada = cl.imageTag('logos/ada2', {effect: "colorize", color: "#000000", height: 20, quality: 100, crop: "scale", format: "png"}).toHtml();
+  var audi = cl.imageTag('logos/audi', {effect: "colorize", color: "#0AC5A8", height: 12, quality: 100, crop: "scale", format: "png"}).toHtml();
+  var getaround = cl.imageTag('logos/getaround', {effect: "colorize", color: "#0AC5A8", height: 30, quality: 100, crop: "scale", format: "png"}).toHtml();
+  var ada = cl.imageTag('logos/ada2', {effect: "colorize", color: "#0AC5A8", height: 20, quality: 100, crop: "scale", format: "png"}).toHtml();
   var enterprise = cl.imageTag('logos/enterprise2', {transformation: [
-    {effect: "make_transparent:66"},{effect: "colorize", color: "#000000", height: 30, quality: 100, crop: "scale", format: "png"}]}).toHtml();
+    {effect: "make_transparent:66"},{effect: "colorize", color: "#0AC5A8", height: 30, quality: 100, crop: "scale", format: "png"}]}).toHtml();
 
   //#0AC5A8 #F76262 #506690 #293462 #FFF1C1
 
 document.getElementById("imagesjb").innerHTML = audi + getaround + ada + enterprise ;
- document.getElementById("imagegetaround").innerHTML = getaround
  
   
 })();
